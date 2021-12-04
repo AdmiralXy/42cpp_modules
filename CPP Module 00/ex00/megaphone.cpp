48ce845c940bd28919fbf74ad062f2ca323b001a
AdmiralXy <gameproblem80@gmail.com>
2021-12-04 14:58:24 +0300

    fix ex00 implementation

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -2,5 +2,10 @@
 
 int	main(int argc, char **argv)
 {
-	std::cin
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+	std::cout << std::endl;
 }
