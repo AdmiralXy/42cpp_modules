ad746cbe01f3e53f4d496e54bda373fa28572d21
AdmiralXy <gameproblem80@gmail.com>
2022-01-22 04:03:36 +0300

    add tests for operators

--- CPP Module 02/ex02/main.cpp
+++ CPP Module 02/ex02/main.cpp
@@ -2,14 +2,37 @@
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b(Fixed(5.05f) * Fixed(2));
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
+	{
+		Fixed a;
+		Fixed const b(Fixed(5.05f) * Fixed(2));
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max(a, b) << std::endl;
+	}
+	{
+		Fixed A(10.5f);
+		Fixed B(5.3f);
+		Fixed C = A + 5;
+		Fixed D = C;
+		std::cout << "Operators test: " << std::endl;
+		std::cout << "A = " << A << std::endl;
+		std::cout << "B = " << B << std::endl;
+		std::cout << "C = " << C << std::endl;
+		std::cout << "D = " << D << std::endl;
+		std::cout << "A > B: " << (A > B) << std::endl;
+		std::cout << "A < B: " << (A < B) << std::endl;
+		std::cout << "A >= B: " << (A >= B) << std::endl;
+		std::cout << "A <= B: " << (A <= B) << std::endl;
+		std::cout << "C == D: " << (C == D) << std::endl;
+		std::cout << "A != B: " << (A != B) << std::endl;
+		std::cout << "A + C: " << (A + C) << std::endl;
+		std::cout << "A - C: " << (A - C) << std::endl;
+		std::cout << "A * D: " << (A * D) << std::endl;
+		std::cout << "A / D: " << (A / D) << std::endl;
+	}
 	return 0;
 }
